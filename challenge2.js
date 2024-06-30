08eae2a5fca6e88dacf59c9001631e494837ed51
NaomiNyakaru <naomi.nyakaru@gmail.com>
2024-06-30 16:04:39 +0300

    speed detector

--- challenge2.js
+++ challenge2.js
@@ -3,6 +3,23 @@ const ps = require("prompt-sync");
 const prompt = ps()
 
 function speedDetector(){
-    
+    let speed = prompt ("Speed?");
+
+    const speedLimit = 70;
+    const demeritPointPerkm = 5;
+
+    if (speed < speedLimit){
+        return 'OK'
+    }else {
+        const demeritPoints = Math.floor((speed-speedLimit)/demeritPointPerkm);
+        if (demeritPoints > 12 ){
+            return 'Licence suspended'
+        }else{
+            return `Points: ${demeritPoints}`
+        }
+
+    }
 
 }
+
+console.log(speedDetector());
