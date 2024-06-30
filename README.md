c2eab6ee046adfafc5f33417bca8690bbb571fe0
NaomiNyakaru <naomi.nyakaru@gmail.com>
2024-06-30 20:52:23 +0300

    net salary calculator

---
+++ README.md
@@ -0,0 +1,22 @@
+*Challenge1*
+The program is a student grade generator.
+It contains a function -grades that calculates the grades depending on the marks, function(grades) is called at the end and it display the grade using console.log.
+There is also a -prompt that enables user input.
+The grade ranges are as below:
+ A > 79
+ B - 60 to 79
+ C - 59 to 49
+ D - 40 to 48
+ E - less 40.
+
+*Challenge2*
+The program is about a speed generator.
+The program contains a function -speedDetector that calculates demeritPoints to be given to the driver and determine the licence status.
+There is a prompt to enable user input.
+From the body of the function: SpeedLimit is placed at 70 and a demerit point for every other 5km
+If the speed is below 70, an OK is returned. 
+If it is above 70 demeritPoints are calculated by subtracting it speed limit and then dividing the difference by 5.
+The demerit points are further use to determine licence status.
+If the demerits are above 12 the licence is suspended, otherwise the demerit points are displayed.
+
+*Challange3*
--- challenge3.js
+++ challenge3.js
@@ -1,1 +1,66 @@
 /////Net salary calculator
+const ps = require("prompt-sync");
+const prompt = ps()
+
+
+function grossSalaryCalculation (){
+    let basicSalary = prompt("Enter basic salary: ");
+    let benefits = prompt("Enter benefits: ");
+    basicSalary = parseFloat(basicSalary);
+    benefits = parseFloat(benefits);
+    const grossSalary = basicSalary + benefits;
+    return ` gross Salary: ${ grossSalary}`
+}
+
+function taxDeductions(grossSalary){
+    
+    if (grossSalary <= 24000){
+        const tax = grossSalary * 0.1
+        return `Tax: ${tax}`
+    }else if (grossSalary >= 24001 && grossSalary <= 32333){
+        const tax = grossSalary * 0.25
+        return `Tax: ${tax}`
+    }else if (grossSalary >= 32334 && grossSalary <= 500000){
+        const tax = grossSalary * 0.3
+        return `Tax: ${tax}`
+    }else if ( grossSalary >= 500001 && grossSalary <= 800000){
+        const tax = grossSalary * 0.325
+        return `Tax: ${tax}`
+    }else {
+        const tax = grossSalary * 0.35
+        return `Tax: ${tax}`
+    }
+}
+
+function nhifDeductionsCalculator(grossSalary){
+    
+      const nhifDeductions = 0.025 * grossSalary
+      return `NHIF deductions: ${ nhifDeductions}`
+}
+
+function nssfDeductionsCalculator(grossSalary){
+   
+    const nssfDeductions = 0.06 * grossSalary
+      return `NSSF deductions: ${ nssfDeductions}`
+}
+
+function netSalaryCalculation(grossSalary, tax , nhifDeductions,nssfDeductions){
+   
+    const netSalary = grossSalary - tax- nssfDeductions - nhifDeductions
+    return `netSalary: ${ netSalary}`
+}
+
+let grossSalary = parseFloat(grossSalaryCalculation().split(": ")[1]);
+console.log(`Gross Salary: ${grossSalary}`);
+
+let tax = parseFloat(taxDeductions(grossSalary).split(": ")[1]);
+console.log(`Tax: ${tax}`);
+
+let nhif = parseFloat(nhifDeductionsCalculator(grossSalary).split(": ")[1]);
+console.log(`NHIF deductions: ${nhif}`);
+
+let nssf = parseFloat(nssfDeductionsCalculator(grossSalary).split(": ")[1]);
+console.log(`NSSF deductions: ${nssf}`);
+
+let netSalary = parseFloat(netSalaryCalculation(grossSalary, tax, nhif, nssf).split(": ")[1]);
+console.log(`Net Salary: ${netSalary}`);
